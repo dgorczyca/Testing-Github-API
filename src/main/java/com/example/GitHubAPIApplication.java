27363221ccc2a4a66eac7c51e6c08fb5e204291e
Dawid Gorczyca <vidda22@gmail.com>
2017-01-30 18:48:53 +0000

    Initial commit, adding basic Maven structure for the SpringBoot Application

---
+++ src/main/java/com/example/GitHubAPIApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubAPIApplication.class, args);
+	}
+}
